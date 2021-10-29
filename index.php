8152276656af5b9931f965c8ad59aec6a51815ff
jakekviti <thevitij64@gmail.com>
2021-10-29 16:21:54 -0400

    Laid out basic index page

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hey!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Google Clone</title>
+</head>
+<body>
+    
+
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://pbs.twimg.com/media/Eht3FIKUYAIymZP?format=png&name=240x240">
+            <div class="searchContainer">
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
