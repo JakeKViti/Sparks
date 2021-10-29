6b58e5577e90e915f08593c121ed6db43aa0951f
jakekviti <thevitij64@gmail.com>
2021-10-29 18:01:49 -0400

    Create search.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Google Clone</title>
+    <title>Sparks</title>
 </head>
 <body>
-    
-
     <div class="mainSection">
         <div class="logoContainer">
             <img src="https://pbs.twimg.com/media/Eht3FIKUYAIymZP?format=png&name=240x240">
             <div class="searchContainer">
-                <input class="searchBox" type="text" name="term">
-                <input class="searchButton" type="submit" value="Search">
+                <form action="search.php" method="GET">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
             </div>
         </div>
     </div>
-
 </body>
 </html>
