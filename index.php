5961e24641c45b9b2c68093a58cc0d57031774bf
jakekviti <thevitij64@gmail.com>
2021-10-29 18:13:29 -0400

    Style Homepage

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="https://pbs.twimg.com/media/Eht3FIKUYAIymZP?format=png&name=240x240">
-            <div class="searchContainer">
-                <form action="search.php" method="GET">
-                    <input class="searchBox" type="text" name="term">
-                    <input class="searchButton" type="submit" value="Search">
-                </form>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="https://pbs.twimg.com/media/Eht3FIKUYAIymZP?format=png&name=240x240">
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <input class="searchBox" type="text" name="term">
+                        <input class="searchButton" type="submit" value="Search">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
