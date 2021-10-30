ba356b40d0cf9fe40a8ecff473ba435f359adb0e
jakekviti <thevitij64@gmail.com>
2021-10-30 18:12:52 -0400

    Build out search.php

--- search.php
+++ search.php
@@ -1,5 +1,30 @@
-<?php
-
-echo $_GET["term"]
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sparks</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <a herf="index.php">
+                        <img src="assets/images/sparksLogo.png" >
+                    </a>
+                </div>
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <div class="searchBarContainer">
+                        <input class="searchBox" type="text" name="term">
+                        <button>
+                            Search
+                        </button>
+                    </div>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
