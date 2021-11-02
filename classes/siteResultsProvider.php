df4f3f3d57a8fabc7fbbb2996f350745da3ab6ca
jakekviti <thevitij64@gmail.com>
2021-11-02 14:27:33 -0400

    Search Results now show title, url, and description

--- classes/siteResultsProvider.php
+++ classes/siteResultsProvider.php
@@ -36,8 +36,21 @@ class SiteResultsProvider{
 
         $resultsHtml = "<div class='siteResults'>";
         while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $id = $row["id"];
+            $url = $row["url"];
             $title = $row["title"];
-            $resultsHtml .= "$title <br>";
+            $description = $row["description"];
+
+            $resultsHtml .= "<div class='resultContainer>
+                                <h3 class='title'>
+                                    <a class='result' href ='$url'>
+                                        $title
+                                    </a>
+                                </h3>
+                                <span class='url'>$url</span>
+                                <span class='description'>$description</span>
+                            </div>
+                            <br>";
         }
         $resultsHtml .= "</div>";
         return $resultsHtml;
