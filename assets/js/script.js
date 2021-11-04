665290475819f66526fdd6be3ec46a475e9a7805
jakekviti <thevitij64@gmail.com>
2021-11-04 14:11:57 -0400

    Function in scripts gets url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    $(".result").on("click", function(){
+        let url = $(this).attr("href");
+        return false
+    })
 })
+
+function increaseLinkClicks(linkID, url){
+
+}
