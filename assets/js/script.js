2cbfe387897733011c7b6656dcbc24ddd2e40f22
jakekviti <thevitij64@gmail.com>
2021-11-04 16:57:20 -0400

    Site will remember if it is a site search or img search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
     $(".result").on("click", function(){
         let url = $(this).attr("href");
         let linkID = $(this).attr("data-linkID");
+
+        if(!id){
+            alert("DataID is not found!")
+        }
+        increaseLinkClicks(linkID, url);
         return false
     })
 })
 
 function increaseLinkClicks(linkID, url){
-
+    $.post("ajax/updateLinkCount.php", {linkID: linkID});
 }
--- search.php
+++ search.php
@@ -29,6 +29,7 @@ include("classes/siteResultsProvider.php");
             <div class="searchContainer">
                 <form action="search.php" method="GET">
                     <div class="searchBarContainer">
+                        <input type='hidden' name="type" value="<?php echo $type;?>"
                         <input class="searchBox" type="text" name="term" value="<?php echo $term;?>">
                         <button class="searchButton">
                             <img src="assets/images/emblem.png">
