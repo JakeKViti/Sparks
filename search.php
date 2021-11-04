9d58eb8ea2f726eb27a04c1e00049a62bda51390
jakekviti <thevitij64@gmail.com>
2021-11-04 14:07:43 -0400

    Create scirpts.js

--- search.php
+++ search.php
@@ -15,6 +15,7 @@ include("classes/siteResultsProvider.php");
 <head>
     <title>Sparks</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="wrapper">
@@ -105,5 +106,6 @@ include("classes/siteResultsProvider.php");
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
